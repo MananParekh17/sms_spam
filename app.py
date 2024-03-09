0f7ebcff4f4cd76956dc2c183ac9fa6c6eef19fa
MananParekh17 <mananparekh1755@gmail.com>
2024-03-10 00:51:17 +0530

    stopwords

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def transform_text(text):
     return " ".join(y)
 
 
-st.title("Email/SMS Spam Classifier")
+st.title("Email/SMS hhh Spam Classifier")
 inpt = st.text_area("Enter the message")
 
 if st.button('Predict'):
